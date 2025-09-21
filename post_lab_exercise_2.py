8f570baeb8ebecfce6470bea0ce3a28dcd067f2e
Man Bhalodiya <mankumar.bhalodiya130005@marwadiuniversity.ac.in>
2025-09-21 18:23:13 +0530

    Update post_lab_exercise_2.py

--- post_lab_exercise_2.py
+++ post_lab_exercise_2.py
@@ -1,6 +1,7 @@
 
 from PIL import Image, ImageOps
 
-img = Image.open(r'D:\MU.jpg')
+img = Image.open(r'C:\Users\Man Bhalodiya\Desktop\t-1648985762-marwadi-university-mu-rajkot-rajkot.jpeg')
 padded = ImageOps.expand(img, border=20, fill='black')
+
 padded.show()
